ff6c1d2162337a85c6e1135852c56cf6b807b5fb
glima-de <glima-de@student.42sp.org.br>
2021-07-18 14:05:54 +0000

    rush04

---
+++ rush04.c
@@ -0,0 +1,29 @@
+void	ft_putchar(char c);
+
+void	rush(int x, int y)
+{
+	int	yi;
+	int	xi;
+
+	yi = 0;
+	while (yi < y && y >= 0)
+	{
+		xi = 0;
+		while (xi < x && x >= 0)
+		{
+			if ((yi == 0 && xi == 0))
+				ft_putchar('A');
+			else if (yi == y - 1 && xi == x - 1 && y > 1 && x > 1)
+				ft_putchar('A');
+			else if ((yi == 0 && xi == x - 1) || (yi == y - 1 && xi == 0))
+				ft_putchar('C');
+			else if (yi == 0 || yi == y - 1 || xi == 0 || xi == x - 1)
+				ft_putchar('B');
+			else
+				ft_putchar(' ');
+			xi++;
+		}
+		yi++;
+		ft_putchar('\n');
+	}
+}
