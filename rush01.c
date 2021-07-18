b3fb3a9784df643a9156568ac8ab0346a22fa76d
glima-de <glima-de@student.42sp.org.br>
2021-07-18 13:40:30 +0000

    rush01

---
+++ rush01.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+void	ft_putchar(char c);
+
+void	rush(int x, int y)
+{
+	int	yi;
+	int	xi;
+
+	yi = 0;
+	while (yi < y && y > 0 && x > 0)
+	{
+		xi = 0;
+		while (xi < x)
+		{
+			if ((yi == 0 && xi == 0))
+				ft_putchar('/');
+			else if (yi == y - 1 && xi == x - 1 && y > 1 && x > 1)
+				ft_putchar('/');
+			else if ((yi == 0 && xi == x - 1) || (yi == y - 1 && xi == 0))
+				ft_putchar('\\');
+			else if (yi == 0 || yi == y - 1 || xi == 0 || xi == x - 1)
+				ft_putchar('*');
+			else
+				ft_putchar(' ');
+			xi++;
+		}
+		yi++;
+		ft_putchar('\n');
+	}
+}
