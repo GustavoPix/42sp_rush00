3feada3bdd124a752cbc9fd4f46ab7d684bd76d3
glima-de <glima-de@student.42sp.org.br>
2021-07-18 13:50:46 +0000

    rush 02

---
+++ rush02.c
@@ -0,0 +1,27 @@
+void	ft_putchar(char c);
+
+void	rush(int x, int y)
+{
+	int	yi;
+	int	xi;
+
+	yi = 0;
+	while (yi < y && y >= 0)
+	{
+		xi = 0;
+		while (xi < x && x >= 0)
+		{
+			if (yi == 0 && (xi == 0 || xi == x - 1))
+				ft_putchar('A');
+			else if (yi == y - 1 && (xi == 0 || xi == x - 1))
+				ft_putchar('C');
+			else if (yi == 0 || yi == y - 1 || xi == 0 || xi == x - 1)
+				ft_putchar('B');
+			else
+				ft_putchar(' ');
+			xi++;
+		}
+		yi++;
+		ft_putchar('\n');
+	}
+}
